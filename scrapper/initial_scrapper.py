a96ec2d95752110ca10bee71e0577d1c6fc4ca82
Charles Ivia <iviacharles@gmail.com>
2021-07-01 11:08:03 +0300

    scrap data

---
+++ scrapper/initial_scrapper.py
@@ -0,0 +1,47 @@
+# This script Kenyan scraps startups from startupranking.com
+
+# Import required libraries
+
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup as soup
+from urllib.request import Request, urlopen
+
+# URLs
+
+first_page = "https://www.startupranking.com/top/kenya"
+second_page = "https://www.startupranking.com/top/kenya/2"
+third_page = "https://www.startupranking.com/top/kenya/3"
+
+# Function to read data from
+
+
+def read_data(url):
+    """Disguise our request by sending a HTTP header indicating the request was being sent from the Firefox web browser"""
+    req = Request(url, headers={"User-agent": "Mozilla/5.0"})
+    webpage = urlopen(req).read()
+    tables = pd.read_html(webpage)
+    """Check number of tables"""
+    print(len(tables))
+    return tables
+
+
+# Call the function and create dataframes
+
+first_df = read_data(first_page)[0]
+print(first_df)
+
+second_df = read_data(second_page)[0]
+print(second_df)
+
+third_df = read_data(third_page)[0]
+print(third_df)
+
+
+dataframes = [first_df, second_df, third_df]
+
+# Combine dataframes into a single dataframe
+
+df = pd.concat(dataframes, axis=0, ignore_index=True)
+
+print(f"Cobined dataframe {df}")
