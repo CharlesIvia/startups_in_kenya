bd0c0d6e6a789dae4ab6c85ce28dc1b08457c56d
Charles Ivia <iviacharles@gmail.com>
2021-07-02 13:29:18 +0300

    add afribon and afya_access data

---
+++ merge.py
@@ -0,0 +1,25 @@
+# Required libraries
+import pandas as pd
+from startup_scripts import access_afya, afribon
+
+# Read-in Data
+
+df = pd.read_csv("./data/startups.csv", index_col="Startup")
+df = df.drop("Unnamed: 0", axis=1)
+
+# Create additional columns
+df[["Founders", "Year Founded", "Website", "Status"]] = None
+print(df)
+print(df.columns)
+
+# Access Afya
+access_afya.access_afya_data(df)
+print(df)
+
+# Afribon
+afribon.afribon_data(df)
+print(df)
+
+# Save dataframe
+
+df.to_csv("./data/main.csv")
